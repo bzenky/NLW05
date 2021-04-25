40d03850243287e3359c53a6daa0dcfc8fe8f790
Zenky <bzenky@gmail.com>
2021-04-25 10:16:36 -0300

    Finalizando a NLW.
    Terminando todo o app e a NLW. Necessita de ajustes para ser responsivo.

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -1,13 +1,14 @@
 import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR'
 import { GetStaticProps, GetStaticPaths } from 'next';
-import { useRouter } from 'next/router'
 import { api } from '../../services/api';
 import Link from 'next/link';
 import Image from 'next/image';
 import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
 
 import styles from './episode.module.scss';
+import { usePlayer } from '../../contexts/PlayerContext';
+import Head from 'next/head';
 
 type Episode = {
   id: string;
@@ -26,8 +27,14 @@ type EpisodeProps = {
 }
 
 export default function Episode({ episode }: EpisodeProps) {
+  const { play } = usePlayer();
+
   return (
     <div className={styles.episode}>
+      <Head>
+        <title>{episode.title} | Podcastr</title>
+      </Head>
+      
       <div className={styles.thumbnailContainer}>
         <Link href="/">
           <button type="button">
@@ -42,7 +49,7 @@ export default function Episode({ episode }: EpisodeProps) {
           objectFit="cover"
         />
 
-        <button type="button">
+        <button type="button" onClick={() => play(episode)}>
           <img src="/play.svg" alt="Tocar Episódio" />
         </button>
 
